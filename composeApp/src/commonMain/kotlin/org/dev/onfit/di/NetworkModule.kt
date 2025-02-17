928f3850bbc414949cc91c4cd5d3931d7a75d8ff
Federico Salgado <federicosalgado2002@gmail.com>
2025-02-16 23:44:14 -0300

    Add: network, datasource and repository modules - setup Ktor client

---
+++ composeApp/src/commonMain/kotlin/org/dev/onfit/di/NetworkModule.kt
@@ -0,0 +1,35 @@
+package org.dev.onfit.di
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.dev.onfit.data.auth.AuthRemoteDataSource
+import org.dev.onfit.data.auth.AuthRepository
+import org.koin.dsl.module
+
+val repositoryModule = module {
+    single<AuthRepository> { AuthRepository(get<AuthRemoteDataSource>(),get<AuthRemoteDataSource>()) }
+}
+
+val dataSourceModule = module {
+    single<AuthRemoteDataSource> { AuthRemoteDataSource(get()) }
+}
+
+val networkModule = module {
+    single {
+        HttpClient {
+            defaultRequest {
+                url("https://api.onfit.dev/v1/")
+            }
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+}
