80da221644246806ca1d2e74b807ff1bc8681737
Federico Salgado <federicosalgado2002@gmail.com>
2025-02-27 20:15:48 -0300

    Refactor: Update bottom navigation icons
    This commit updates the icons used in the bottom navigation bar:
    - Replaces `Icons.Rounded.Person` with `Icons.Outlined.Person` for the Profile item.
    - Replaces `Icons.Rounded.ShoppingBag` with `Icons.Outlined.ShoppingBag` for the Shop item.

--- composeApp/src/commonMain/kotlin/org/dev/onfit/ui/home/ItemsBottomNav.kt
+++ composeApp/src/commonMain/kotlin/org/dev/onfit/ui/home/ItemsBottomNav.kt
@@ -1,8 +1,8 @@
 package org.dev.onfit.ui.home
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material.icons.rounded.ShoppingBag
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.ShoppingBag
 import androidx.compose.ui.graphics.vector.ImageVector
 import org.dev.onfit.ui.navigation.HomeDestination
 
@@ -12,13 +12,13 @@ sealed class ItemsBottomNav(
     val route: HomeDestination
 ) {
     data object Profile: ItemsBottomNav(
-        icon = Icons.Rounded.Person,
+        icon = Icons.Outlined.Person,
         title = "Profile",
         route = HomeDestination.Profile
     )
 
     data object Shop: ItemsBottomNav(
-        icon = Icons.Rounded.ShoppingBag,
+        icon = Icons.Outlined.ShoppingBag,
         title = "Shop",
         route = HomeDestination.Shop
     )
