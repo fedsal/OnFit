35a09e26bfc69fd49f720056e08ae25286c634a8
Federico Salgado <federicosalgado2002@gmail.com>
2025-03-01 17:47:52 -0300

    feat: Create ProfileCard Composable and integrate it into HomeScreen
    -   Created a new `ProfileCard` composable to display user profile information within a card.
    -   Implemented the `ProfileCard` with user name, plan details, and a dropdown icon.
    -   Moved the `ProfileCard` logic from `HomeScreen` into the `ProfileCard` composable.
    -   Added a logo image inside the `ProfileCard`.
    -   Integrated the `ProfileCard` into the `HomeScreen` layout.

---
+++ composeApp/src/commonMain/kotlin/org/dev/onfit/ui/home/composables/ProfileCard.kt
@@ -0,0 +1,64 @@
+package org.dev.onfit.ui.home.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import onfit.composeapp.generated.resources.Res
+import onfit.composeapp.generated.resources.logo_icon
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun ProfileCard() {
+    Card(
+        Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp).height(IntrinsicSize.Min),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(Res.drawable.logo_icon),
+                modifier = Modifier.size(50.dp),
+                contentDescription = null
+            )
+            Spacer(Modifier.width(16.dp))
+            Column {
+                Text("Federico Salgado", color = Color.Black, fontSize = 18.sp, style = TextStyle(fontWeight = FontWeight.Bold))
+                Spacer(Modifier.size(8.dp))
+                Text("Plan: Vidriera 12 meses")
+                Text("Vencimiento: 02/08/2025")
+            }
+            Spacer(Modifier.size(8.dp))
+            Box(Modifier.weight(1f).fillMaxHeight(), contentAlignment = Alignment.BottomEnd) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = ""
+                )
+            }
+        }
+    }
+}
