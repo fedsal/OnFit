116cd44a53a00f5b7796ebeb3dcced247149e3b6
Federico Salgado <federicosalgado2002@gmail.com>
2025-03-03 19:56:39 -0300

    Refactor: Update Profile Card to Profile Section and add new UI elements
    - Renamed `ProfileCard` to `ProfileSection`.
    - Moved the `ProfileCard` composable inside the new `ProfileSection` composable.
    - Added a new card to `ProfileSection` to display declaration information.
    - Updated `HomeScreen` to use `ProfileSection` instead of `ProfileCard`.
    - Added QrCode icon to `ProfileCard`
    - Added a favorite icon to the new card.

--- composeApp/src/commonMain/kotlin/org/dev/onfit/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/dev/onfit/ui/home/HomeScreen.kt
@@ -13,14 +13,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.dev.onfit.ui.home.composables.ProfileCard
+import org.dev.onfit.ui.home.composables.ProfileSection
 import org.dev.onfit.ui.home.composables.SportCenterCard
 
 @Composable
 fun HomeScreen() {
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(Modifier.padding(20.dp)) {
-            ProfileCard()
+            ProfileSection()
             Spacer(Modifier.size(16.dp))
             Text("Tu sede:", color = Color.DarkGray, fontWeight = FontWeight.SemiBold, fontSize = 18.sp)
             Spacer(Modifier.size(8.dp))
--- composeApp/src/commonMain/kotlin/org/dev/onfit/ui/home/composables/ProfileSection.kt
+++ composeApp/src/commonMain/kotlin/org/dev/onfit/ui/home/composables/ProfileSection.kt
@@ -13,7 +13,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.QrCode
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -28,7 +29,35 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import onfit.composeapp.generated.resources.Res
 import onfit.composeapp.generated.resources.logo_icon
+import org.dev.onfit.ui.theme.errorContainerLightMediumContrast
 import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun ProfileSection() {
+    Column {
+        ProfileCard()
+        Spacer(Modifier.height(16.dp))
+        Card(
+            Modifier.fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+        ) {
+            Row(Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    modifier = Modifier.size(35.dp),
+                    imageVector = Icons.Filled.Favorite,
+                    contentDescription = "",
+                    tint = errorContainerLightMediumContrast
+                )
+                Spacer(Modifier.width(16.dp))
+                Column(Modifier.fillMaxWidth().weight(1f)) {
+                    Text("Declaracion jurada", style = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold, fontSize = 18.sp))
+                    Text("Vencimiento: 01/08/2025", style = TextStyle(color = Color.DarkGray, fontSize = 14.sp))
+
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun ProfileCard() {
@@ -53,9 +82,9 @@ fun ProfileCard() {
                 Text("Vencimiento: 02/08/2025")
             }
             Spacer(Modifier.size(8.dp))
-            Box(Modifier.weight(1f).fillMaxHeight(), contentAlignment = Alignment.BottomEnd) {
+            Box(Modifier.weight(1f).fillMaxHeight(), contentAlignment = Alignment.TopEnd) {
                 Icon(
-                    imageVector = Icons.Default.KeyboardArrowDown,
+                    imageVector = Icons.Default.QrCode,
                     contentDescription = ""
                 )
             }
