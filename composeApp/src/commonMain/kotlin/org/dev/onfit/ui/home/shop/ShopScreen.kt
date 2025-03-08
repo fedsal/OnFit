bbb1a6a7dfe96662a0fdd9068c011c7cc19f98ff
Federico Salgado <federicosalgado2002@gmail.com>
2025-03-08 16:13:03 -0300

    feat: Intercept and open WhatsApp and Instagram links in external apps
    -   Added a `RequestInterceptor` to the `WebView` in `ShopScreen`.
    -   Intercepts requests to open WhatsApp and Instagram links.
    -   Opens intercepted links using the device's default application via `LocalUriHandler`.
    -   Rejects intercepted links from loading inside the `WebView`.

--- composeApp/src/commonMain/kotlin/org/dev/onfit/ui/home/shop/ShopScreen.kt
+++ composeApp/src/commonMain/kotlin/org/dev/onfit/ui/home/shop/ShopScreen.kt
@@ -7,22 +7,44 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
+import com.multiplatform.webview.request.RequestInterceptor
+import com.multiplatform.webview.request.WebRequest
+import com.multiplatform.webview.request.WebRequestInterceptResult
 import com.multiplatform.webview.web.LoadingState
 import com.multiplatform.webview.web.WebView
+import com.multiplatform.webview.web.WebViewNavigator
+import com.multiplatform.webview.web.rememberWebViewNavigator
 import com.multiplatform.webview.web.rememberWebViewState
 
 @Composable
 fun ShopScreen() {
+    val urlHandler = LocalUriHandler.current
     Surface(modifier = Modifier.fillMaxSize()) {
         Box(contentAlignment = Alignment.Center) {
             val state = rememberWebViewState("https://tienda.onfit.com.ar/")
+            val webViewNavigator = rememberWebViewNavigator(
+                requestInterceptor = object: RequestInterceptor {
+                    override fun onInterceptUrlRequest(
+                        request: WebRequest,
+                        navigator: WebViewNavigator
+                    ): WebRequestInterceptResult {
+                        if (request.url.contains("whatsapp") || request.url.contains("instagram")) {
+                            urlHandler.openUri(request.url)
+                            return WebRequestInterceptResult.Reject
+                        }
+                        return WebRequestInterceptResult.Allow
+                    }
+                }
+            )
             val loadingState = state.loadingState
             if (loadingState is LoadingState.Loading) {
                 CircularProgressIndicator()
             }
             WebView(
                 modifier = Modifier.fillMaxSize(),
-                state = state
+                state = state,
+                navigator = webViewNavigator
             )
         }
     }
